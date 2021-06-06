8a7842b820f47865d14f5a6175b0b77c769270fc
Todd <todd@Todds-MacBook-Air.local>
2021-06-06 10:30:24 +0800

    First 'apis' python CLI commit

---
+++ apis_cli.py
@@ -0,0 +1,8 @@
+import argparse
+
+def apis():
+    """A CLI wrapper for the API of Public APIs."""
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Add some integers')
